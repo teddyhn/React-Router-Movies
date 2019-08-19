b79a27d26115a61898f2b872fb6bb28288eec5a6
teddyhn <tngo97@gmail.com>
2019-08-19 14:43:36 -0700

    Added Save Movie functionality

--- client/src/App.js
+++ client/src/App.js
@@ -7,16 +7,26 @@ import SavedList from './Movies/SavedList';
 
 const App = () => {
   const [savedList, setSavedList] = useState( [] );
+  const [movieInList, setMovieInList] = useState();
 
   const addToSavedList = movie => {
-    setSavedList( [...savedList, movie] );
+    const findMovie = savedList.find(el => movie.id === el.id);
+    if (findMovie) {
+      setMovieInList(`Movie already saved.`);
+      setTimeout(() => setMovieInList(null), 2000);
+    } else setSavedList( [...savedList, movie] );
   };
 
   return (
     <div>
+      {movieInList !== null ? (
+        <h3 className="movie-warning">{movieInList}</h3>
+      ) : null}
       <SavedList list={savedList} />
       <Route exact path="/" component={MovieList} />
-      <Route path="/movies/:id" component={Movie} />
+      <Route path="/movies/:id" render={(props) => {
+        return(<Movie {...props} addToSavedList={addToSavedList}/>)
+      }} />
     </div>
   );
 };
--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -22,11 +22,11 @@ const Movie = (props) => {
 
   },[]);
   
-  // Uncomment this only when you have moved on to the stretch goals
-  // const saveMovie = () => {
-  //   const addToSavedList = props.addToSavedList;
-  //   addToSavedList(movie)
-  // }
+  
+  const saveMovie = () => {
+    const addToSavedList = props.addToSavedList;
+    addToSavedList(movie)
+  }
 
   if (!movie) {
     return <div>Loading movie information...</div>;
@@ -42,7 +42,7 @@ const Movie = (props) => {
         metascore={metascore}
         stars={stars}
       />
-      <div className="save-button">Save</div>
+      <div className="save-button" onClick={() => saveMovie()}>Save</div>
     </div>
   );
 }
