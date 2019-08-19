e94c3c3ebafea3bc010912fc1c55392466842992
teddyhn <tngo97@gmail.com>
2019-08-19 13:33:42 -0700

    MVP completed

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -43,7 +43,8 @@ const Movie = (props) => {
           Metascore: <strong>{metascore}</strong>
         </div>
         <h3>Actors</h3>
-        {stars.map(star => (
+
+        {stars && stars.map(star => (
           <div key={star} className="movie-star">
             {star}
           </div>
