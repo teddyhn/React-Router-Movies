0b518708e7e76eb2f74ec622ebc13703cc694905
teddyhn <tngo97@gmail.com>
2019-08-19 13:45:46 -0700

    Stretch completed: refactored code for DRYness

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
+import MovieCard from './MovieCard';
+
 const Movie = (props) => {
   const [movie, setMovie] = useState({});
 
@@ -34,22 +36,12 @@ const Movie = (props) => {
 
   return (
     <div className="save-wrapper">
-      <div className="movie-card">
-        <h2>{title}</h2>
-        <div className="movie-director">
-          Director: <em>{director}</em>
-        </div>
-        <div className="movie-metascore">
-          Metascore: <strong>{metascore}</strong>
-        </div>
-        <h3>Actors</h3>
-
-        {stars && stars.map(star => (
-          <div key={star} className="movie-star">
-            {star}
-          </div>
-        ))}
-      </div>
+      <MovieCard 
+        title={title}
+        director={director}
+        metascore={metascore}
+        stars={stars}
+      />
       <div className="save-button">Save</div>
     </div>
   );
--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
 
+import MovieCard from './MovieCard';
+
 const MovieList = props => {
   const [movies, setMovies] = useState([])
   useEffect(() => {
@@ -32,22 +34,12 @@ function MovieDetails({ movie }) {
   const { title, director, metascore, stars } = movie;
   return (
     <Link to={`/movies/${movie.id}`}>
-      <div className="movie-card">
-        <h2>{title}</h2>
-        <div className="movie-director">
-          Director: <em>{director}</em>
-        </div>
-        <div className="movie-metascore">
-          Metascore: <strong>{metascore}</strong>
-        </div>
-        <h3>Actors</h3>
-
-        {stars.map(star => (
-          <div key={star} className="movie-star">
-            {star}
-          </div>
-        ))}
-      </div>
+      <MovieCard 
+        title={title}
+        director={director}
+        metascore={metascore}
+        stars={stars}
+      />
     </Link>
   );
 }
